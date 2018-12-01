8a4c88f53e558dece02184b2e75bd67b083567ae
Alex <alhryb@gmail.com>
2018-11-30 22:08:48 -0500

    fixed package.json

--- home-game.js
+++ home-game.js
@@ -6,24 +6,13 @@ const fs = require('fs');
 const url = require('url');
 const path = require('path');
 const express = require('express');
-const serveStatic = require('serve-static');
 
-var port = 8080;
-var webModules = [
-  'phaser-ce'
-];
+let port = 8080;
 
-console.log('Starting server on port ' + port);
+console.log(`Running Home on port ${port}`);
 
-var app = express();
-var server = http.createServer(app);
-
-webModules.forEach(modName => {
-	var p = require.resolve(modName);
-	p = path.dirname(p);
-	console.log(p);
-	app.use(`/lib/${modName}`, serveStatic(p));
-});
+let app = express();
+let server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname,'www')));
 
